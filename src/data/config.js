d067dec7cf39b4a74ca0eb9a3cd5c0c6d83ddf03
Yaroslav <86706552+BeautifulBabe-MTM@users.noreply.github.com>
2025-08-11 23:13:52 +0300

    Update config.js

--- src/data/config.js
+++ src/data/config.js
@@ -1,3 +1,3 @@
 // https://packages-server-75ra.onrender.com
 // http://localhost:5000
-export const API_URL = "http://localhost:5000";
+export const API_URL = "https://packages-server-75ra.onrender.com";
